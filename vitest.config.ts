12b82a739bc4fea26ed6a073ab4b3245db4cf945
Gustavo Carbonera <gustavo.carbonera@atplus.com.br>
2026-07-23 16:47:16 -0300

    chore: set vitest coverage thresholds to 85%

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,14 @@ export default defineConfig({
 	test: {
 		globals: true,
 		include: ["src/tests/**/*.test.ts"],
+		coverage: {
+			provider: "v8",
+			thresholds: {
+				statements: 85,
+				branches: 85,
+				functions: 85,
+				lines: 85,
+			},
+		},
 	},
 });
